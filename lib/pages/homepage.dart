8ede94bc508e0d56824d3e29ab3e961c401d4939
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-11 22:00:11 +0545

    User profile decoreted

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,12 +1,42 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("E learning app")),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "Hello, ",
+                        style: GoogleFonts.lato(fontSize: 25),
+                      ),
+                      Text("Shakriya",
+                          style: GoogleFonts.actor(
+                              fontSize: 25, fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                  Icon(
+                    CupertinoIcons.search,
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
